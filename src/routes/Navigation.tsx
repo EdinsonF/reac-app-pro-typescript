0fc2a25dd4effab5413ecee786f46b457963c4d3
Edinson F <figueroaedinson@gmail.com>
2022-10-06 18:43:14 -0500

    Router concat lazy

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -7,11 +7,7 @@ import {
   NavLink, 
   Navigate 
 } from 'react-router-dom';
-import { 
-  LazyPage1,
-  LazyPage2,
-  LazyPage3
-} from '../01-lazyload/pages';
+
 
 
 import logo from '../logo.svg';
@@ -51,7 +47,7 @@ const Navigation = () => {
                 />
               ))
             }
-            <Route path="*" element={ <Navigate to="/lazy1" replace /> }/>
+            <Route path="*" element={ <Navigate to={ routes[0].to } replace /> }/>
           </Routes>
           
 
